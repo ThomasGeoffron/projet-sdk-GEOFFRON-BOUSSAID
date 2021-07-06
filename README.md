4debc06dc236eb800f3e6759aece0e8313b9d3c6
Thomas GEOFFRON <tgeoffron@myges.fr>
2021-07-06 05:36:37 -0700

    Provider Microsoft opérationnel

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # projet-sdk-GEOFFRON-BOUSSAID
 Projet SDK pour le 19/07/2021. Fait avec Yanis BOUSSAÏD.
 * Branche discord.
+
+Premièrement, lancez le docker avec la commande `docker-compose up`.
+
+Deuxièmement, rendez-vous à l'URL suivante: `https://localhost/login`.
+
+Vous pouvez maintenant choisir l'un des 4 types de connexion.
--- oauth-3IW2/oauth-client/index.php
+++ oauth-3IW2/oauth-client/index.php
@@ -3,6 +3,8 @@ const CLIENT_ID = "234218832452-vchpu8079urgcmp9askc6cum5oej4au1.apps.googleuser
 const CLIENT_SECRET = "ZlOPVwl1-V39Cc_1OlGx5p8Y";
 const CLIENT_FBID = "4117744681651255";
 const CLIENT_FBSECRET = "754f0d9bfb888e92a6a72450ce95c66f";
+const CLIENT_MICID = "81747747-54c6-4870-baa4-cb5955218b77";
+const CLIENT_MICSECRET = "SC~7MYQ8.pFWD6X9o-1hTpY0_zSlW4y214";
 const STATE = "fdzefzefze";
 
 session_start();
@@ -28,7 +30,14 @@ function handleLogin()
             'state' => $_SESSION['state']
         ]);
     $urlDiscord = "";
-    $urlMicrosoft = "";
+    $urlMicrosoft = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?"
+        . http_build_query([
+            'redirect_uri' => 'https://localhost/redirect-microsoft',
+            'response_type' => 'code',
+            'client_id' => CLIENT_MICID,
+            'scope' => 'https://graph.microsoft.com/user.read',
+            'state' => $_SESSION['state']
+        ]);
 
     echo "<h1>Login with :</h1>";
     echo "<a href='". $urlGoogle ."'><img src='http://assets.stickpng.com/thumbs/5847f9cbcef1014c0b5e48c8.png' width='50px' height='50px'></a>";
@@ -38,11 +47,7 @@ function handleLogin()
         . "&scope=email"
         . "&state=" . $_SESSION['state']
         . "&redirect_uri=https://localhost/fbauth-success'><img src='https://www.freepnglogos.com/uploads/discord-logo-png/concours-discord-cartes-voeux-fortnite-france-6.png' width='50px' height='50px'></a>";
-    echo "<a href='https://www.facebook.com/v2.10/dialog/oauth?response_type=code"
-        . "&client_id=" . CLIENT_FBID
-        . "&scope=email"
-        . "&state=" . $_SESSION['state']
-        . "&redirect_uri=https://localhost/fbauth-success'><img src='https://flowerdocs.com/img/documentation/microsoft.png' width='50px' height='50px'></a>";
+    echo "<a href='". $urlMicrosoft ."'><img src='https://flowerdocs.com/img/documentation/microsoft.png' width='50px' height='50px'></a>";
 
     if (isset($_SESSION['google_user'])) {
         echo '<h2>Salut ' . $_SESSION['google_user'] . " !</h2>";
@@ -54,12 +59,18 @@ function handleLogin()
 
         echo "<a href='/disconnect'>Deconnexion</a>";
     }
+    if (isset($_SESSION['mic_user'])) {
+        echo '<h2>Salut ' . $_SESSION['mic_user'] . " !</h2>";
+
+        echo "<a href='/disconnect'>Deconnexion</a>";
+    }
 }
 
 function handleError()
 {
     ["state" => $state] = $_GET;
-    echo "{$state} : Request cancelled";
+    echo $state . " : Request cancelled";
+    echo "<a href='/login'>Accueil</a>";
 }
 
 function redirectGoogle() {
@@ -111,7 +122,7 @@ function redirectGoogle() {
         die();
 
     } else {
-        echo 'no';
+        header('Location: /auth-cancel');
     }
 
 }
@@ -156,40 +167,58 @@ function redirectFb()
 
         header('Location: /login');
 
+    } else {
+        header('Location: /auth-cancel');
     }
-
-    /*["state" => $state, "code" => $code] = $_GET;
-    if ($state !== STATE) {
-        throw new RuntimeException("{$state} : invalid state");
-    }
-    // https://auth-server/token?grant_type=authorization_code&code=...&client_id=..&client_secret=...
-    $url = "https://graph.facebook.com/oauth/access_token?grant_type=authorization_code&code={$code}&client_id=" . CLIENT_FBID . "&client_secret=" . CLIENT_FBSECRET."&redirect_uri=https://localhost/fbauth-success";
-    $result = file_get_contents($url);
-    $resultDecoded = json_decode($result, true);
-    ["access_token"=> $token] = $resultDecoded;
-    $userUrl = "https://graph.facebook.com/me?fields=id,name,email";
-    $context = stream_context_create([
-        'http' => [
-            'header' => 'Authorization: Bearer ' . $token
-        ]
-    ]);
-    echo file_get_contents($userUrl, false, $context);*/
 }
 
-function getUser($params)
-{
-    $url = "http://oauth-server:8081/token?client_id=" . CLIENT_ID . "&client_secret=" . CLIENT_SECRET . "&" . http_build_query($params);
-    $result = file_get_contents($url);
-    $result = json_decode($result, true);
-    $token = $result['access_token'];
+function redirectMicrosoft() {
+    if (isset($_GET['code'])) {
+        $httpquery = http_build_query([
+            'redirect_uri' => 'https://localhost/redirect-microsoft',
+            'client_id' => CLIENT_MICID,
+            'client_secret' => CLIENT_MICSECRET,
+            'code' => $_GET['code'],
+            'grant_type' => 'authorization_code']);
 
-    $apiUrl = "http://oauth-server:8081/me";
-    $context = stream_context_create([
-        'http' => [
-            'header' => 'Authorization: Bearer ' . $token
-        ]
-    ]);
-    echo file_get_contents($apiUrl, false, $context);
+        $url = "https://login.microsoftonline.com/common/oauth2/v2.0/token?" . $httpquery;
+
+        $context = stream_context_create([
+            'http' => [
+                'method' => 'POST',
+                'user_agent' => 'Microsoft OAuth Client',
+                'header' => 'Accept: application/json\r\n' .
+                    'Content-type: application/x-www-form-urlencoded\r\n' .
+                    'Content-Length: '.strlen($httpquery) . '\r\n',
+                'content' => $httpquery
+            ]
+        ]);
+
+        $response = file_get_contents($url, false, $context);
+        $response = $response ? json_decode($response) : $response;
+        $accessTokenMicrosoft = $response->access_token ?? false;
+
+        if ($accessTokenMicrosoft) {
+            $url = "https://graph.microsoft.com/v1.0/me";
+
+            $context = stream_context_create([
+                'http' => [
+                    'method' => 'GET',
+                    'header' => [
+                        'Authorization: Bearer ' . $accessTokenMicrosoft,
+                    ]
+                ]
+            ]);
+
+            $user = json_decode(file_get_contents($url, false, $context));
+
+            $_SESSION['mic_user'] = $user->displayName;
+
+            header('Location: /login');
+        }
+    } else {
+        header('Location: /auth-cancel');
+    }
 }
 
 /**
@@ -204,33 +233,17 @@ switch ($route) {
     case '/login':
         handleLogin();
         break;
-    case '/auth-success':
-        handleSuccess();
-        break;
     case '/redirect-google':
         redirectGoogle();
+        break;
+    case '/redirect-microsoft':
+        redirectMicrosoft();
         break;
     case '/redirect-fb':
         redirectFb();
         break;
     case '/auth-cancel':
         handleError();
-        break;
-    case '/password':
-        if ($_SERVER['REQUEST_METHOD'] === "GET") {
-            echo '<form method="POST">';
-            echo '<input name="username">';
-            echo '<input name="password">';
-            echo '<input type="submit" value="Submit">';
-            echo '</form>';
-        } else {
-            ["username" => $username, "password" => $password] = $_POST;
-            getUser([
-                'grant_type' => "password",
-                "username" => $username,
-                "password" => $password
-            ]);
-        }
         break;
     case '/disconnect':
         session_destroy();
